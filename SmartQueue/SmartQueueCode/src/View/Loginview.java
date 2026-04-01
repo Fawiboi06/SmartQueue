d79d8942487a3cff0231263870b5c336abfe8fea
Haroon <haroonahmed050704@gmail.com>
2026-04-01 16:35:05 +0200

    fixade syntaxfel för Loginview

--- SmartQueue/SmartQueueCode/src/View/Loginview.java
+++ SmartQueue/SmartQueueCode/src/View/Loginview.java
@@ -1,29 +1,31 @@
 package View;
-import java.swing.*;
+import javax.swing.*;
 import java.awt.*;
 public class Loginview extends JFrame {
     private JTextField usernameField;
     private JPasswordField passwordField;
+    private JButton loginButton;
+
     public Loginview(){
-    setTitle("Login");
-    setSize(300,200);
-    setDefaultCloseOperation(JFrame.Exit_on_close);
-    setLocationRelativeTo(null);
-    JPanel panel=new JPanel(new GridLayout(3,2,10,10));
+        setTitle("Login");
+        setSize(300,200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        JPanel panel=new JPanel(new GridLayout(3,2,10,10));
 
-    panel.add(new JLabel("Username:"));
-    usernameField=new JTextField();
-    panel.add(usernameField);
+        panel.add(new JLabel("Username:"));
+        usernameField=new JTextField();
+        panel.add(usernameField);
 
-    panel.add(new JLabel("Password:"));
-    usernameField=new JTextField();
-    panel.add(passwordField);
+        panel.add(new JLabel("Password:"));
+        usernameField=new JTextField();
+        panel.add(passwordField);
 
-    loginButton=new JButton("Login");
-    panel.add(new JLabel());
-    panel.add(loginButton);
+        loginButton=new JButton("Login");
+        panel.add(new JLabel());
+        panel.add(loginButton);
 
-    add(panel);
+        add(panel);
     }
     public String getUsername(){
         return usernameField.getText();
