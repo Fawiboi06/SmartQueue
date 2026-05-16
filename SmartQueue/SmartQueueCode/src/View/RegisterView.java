4253e8c3c683901e10cba409ae2a73981e51ec5b
Haroon <haroonahmed050704@gmail.com>
2026-05-16 21:39:16 +0200

    la till RegisterView med fält för användarregistrering

---
+++ SmartQueue/SmartQueueCode/src/View/RegisterView.java
@@ -0,0 +1,14 @@
+package View;
+import javax.swing.*;
+
+public class RegisterView extends JDialog {
+
+        private JTextField usernameField;
+        private JPasswordField passwordField;
+        private JTextField fullNameField;
+        private JTextField phoneField;
+        private JTextField emailField;
+        private JComboBox<String> roleBox;
+        private JButton registerButton;
+        private JButton cancelButton;
+}
